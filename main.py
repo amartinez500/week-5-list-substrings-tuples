02235cd20072e7f302bda3487670594878aa998b
Arturo Martinez <113930370+amartinez500@users.noreply.github.com>
2022-09-19 17:15:34 +0000

    learn to list items

--- main.py
+++ main.py
@@ -16,7 +16,19 @@ print(newSentence.rindex("practice"))
 ###############################################Review############################################################
 #####################################challenge 1################################################################
 # Create a list with 5 elements, inside the variable my_list. You can include strings, booleans, numbers, etc.
-my_list = ["bay","ddaa", 34, 56]
+my_list = ["bay","ddaa", 34, 56,78,89, 102]
+#append will adds to the end of the list
+my_list.append("jewelry")
+print(my_list)
+my_list.pop()
+#pop() removes for the end 
+print(my_list)
+my_list.remove("ddaa")
+print(my_list)
+my_list.insert(3,"bobba bobba")
+print(my_list)
+
+
 
 
 # Add the element "motorcycle" to the following list of means of transportation:
